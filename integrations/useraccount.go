99f633cfed0a236e6a1f59e376b1d5cba0d47a4f
semirmahovkic <semirmahovkic@192.168.1.15>
2023-08-24 20:03:28 +0200

    fix repo

--- integrations/useraccount.go
+++ integrations/useraccount.go
@@ -3,16 +3,24 @@ package integrations
 import "github.com/mahalahub/mahala/user"
 
 type UserRepository struct {
+	accounts []user.Account
 }
 
-func NewUserRepository() UserRepository {
-	return UserRepository{}
+func NewUserRepository() *UserRepository {
+	return &UserRepository{}
 }
 
-func (repo UserRepository) GetUserAccount(username string) (user.Account, error) {
+func (repo *UserRepository) GetUserAccount(username string) (user.Account, error) {
+	for _, acc := range repo.accounts {
+		if acc.Username == username {
+			return acc, nil
+		}
+	}
 	return user.Account{}, nil
 }
 
-func (repo UserRepository) AddUserAccount(account user.Account) (user.Account, error) {
-	return user.Account{}, nil
+func (repo *UserRepository) AddUserAccount(account user.Account) (user.Account, error) {
+	account.ID = len(repo.accounts) + 1
+	repo.accounts = append(repo.accounts, account)
+	return account, nil
 }
